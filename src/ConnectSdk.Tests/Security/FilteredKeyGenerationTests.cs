d1c94fa70fab667a5702571d264d1e896d0eacf1
mattfurness <matt.furness@gmail.com>
2015-08-06 17:00:01 +1000

    Support generating keys from queries

---
+++ src/ConnectSdk.Tests/Security/FilteredKeyGenerationTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
+using System.Text;
+using ConnectSdk.Config;
+using ConnectSdk.Querying;
+using ConnectSdk.Security;
+using Xunit;
+
+namespace ConnectSdk.Tests.Security
+{
+    public class FilteredKeyGenerationTests
+    {
+        public class WhenFilteredKeyIsGenerated
+        {
+            [Fact]
+            public void It_should_encrypt_query_that_is_decryptable()
+            {
+                Connect.Initialize(new BasicConfiguration("key", "proj"));
+
+                var queryToEncrypt = Connect.Query("Moo")
+                    .Select(new
+                    {
+                        Prop = Aggregations.Sum("MyProp")
+                    });
+
+                var queryJson = queryToEncrypt.ToString();
+                var masterKey = "2fMSlDSOGtMWH50wffnCscgGMcJGMQ0s";
+                var filteredKey = queryToEncrypt.GenerateFilteredKey(masterKey);
+                var decryptedQueryJson = Decrypter.Decrypt(masterKey, filteredKey);
+
+                Assert.Equal(queryJson, decryptedQueryJson);
+            }
+        }
+    }
+
+    internal static class Decrypter
+    {
+        public static string Decrypt(string masterKey, string key)
+        {
+            using (var encryption = GetEncryption())
+            {
+                var parts = key.Split('-');
+                var iv = parts[0].FromHexString();
+                var data = parts[1].FromHexString();
+
+                encryption.IV = iv;
+                encryption.Key = masterKey.ToBytes();
+
+                using (var decryptor = encryption.CreateDecryptor())
+                {
+                    var decrypted = decryptor.TransformFinalBlock(data, 0, data.Length);
+                    var token = Encoding.UTF8.GetString(decrypted);
+
+                    return token;
+                }
+            }
+        }
+
+        private static SymmetricAlgorithm GetEncryption()
+        {
+            return new RijndaelManaged
+            {
+                KeySize = 256,
+                BlockSize = 128,
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7
+            };
+        }
+    }
+
+    internal static class HexExtensions
+    {
+        public static byte[] FromHexString(this object value)
+        {
+            return SoapHexBinary.Parse(value.ToString()).Value;
+        }
+    }
+
+    internal static class StringExtensions
+    {
+        public static byte[] ToBytes(this string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+    }
+}
---
+++ src/ConnectSdk/Security/FilteredKeyGeneration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConnectSdk.Querying;
+using PCLCrypto;
+
+namespace ConnectSdk.Security
+{
+    public static class FilteredKeyGeneration
+    {
+        public static string GenerateFilteredKey<TResult>(this IQuery<TResult> query, string masterKey)
+        {
+            var aesProvider = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesCbcPkcs7);
+            var masterKeyBytes = System.Text.Encoding.UTF8.GetBytes(masterKey);
+            var queryJsonBytes = System.Text.Encoding.UTF8.GetBytes(query.ToString());
+
+            var masterSymmetricKey = aesProvider.CreateSymmetricKey(masterKeyBytes);
+            var iv = WinRTCrypto.CryptographicBuffer.GenerateRandom(16);
+            var encryptedQueryJson = WinRTCrypto.CryptographicEngine.Encrypt(masterSymmetricKey, queryJsonBytes, iv);
+
+            return $"{BitConverter.ToString(iv).Replace("-", "")}-{BitConverter.ToString(encryptedQueryJson).Replace("-", "")}";
+        }
+    }
+}
