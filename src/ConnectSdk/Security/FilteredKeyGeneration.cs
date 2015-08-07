486cca4b9f1979b96f10745baf8990868d2c7006
mattfurness <matt.furness@gmail.com>
2015-08-07 10:47:37 +1000

    Fix threading issues in initializer tests

--- src/ConnectSdk/Security/FilteredKeyGeneration.cs
+++ src/ConnectSdk/Security/FilteredKeyGeneration.cs
@@ -6,17 +6,32 @@ namespace ConnectSdk.Security
 {
     public static class FilteredKeyGeneration
     {
-        public static string GenerateFilteredKey<TResult>(this IQuery<TResult> query, string masterKey)
+        public static string GenerateFilteredKey(string query, string masterKey)
         {
             var aesProvider = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesCbcPkcs7);
-            var masterKeyBytes = System.Text.Encoding.UTF8.GetBytes(masterKey);
-            var queryJsonBytes = System.Text.Encoding.UTF8.GetBytes(query.ToString());
+            var masterKeyBytes = masterKey.AsBytes();
+            var queryJsonBytes = query.AsBytes();
 
             var masterSymmetricKey = aesProvider.CreateSymmetricKey(masterKeyBytes);
             var iv = WinRTCrypto.CryptographicBuffer.GenerateRandom(16);
             var encryptedQueryJson = WinRTCrypto.CryptographicEngine.Encrypt(masterSymmetricKey, queryJsonBytes, iv);
 
-            return $"{BitConverter.ToString(iv).Replace("-", "")}-{BitConverter.ToString(encryptedQueryJson).Replace("-", "")}";
+            return $"{iv.AsHex()}-{encryptedQueryJson.AsHex()}";
+        }
+
+        public static string GenerateFilteredKey<TResult>(this IQuery<TResult> query, string masterKey)
+        {
+            return GenerateFilteredKey(query.ToString(), masterKey);
+        }
+
+        private static byte[] AsBytes(this string @string)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(@string);
+        }
+
+        private static string AsHex(this byte[] bytes)
+        {
+            return BitConverter.ToString(bytes).Replace("-", "");
         }
     }
 }
